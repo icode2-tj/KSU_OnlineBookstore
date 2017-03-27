f1c4200cf145b6dcd84ecfce00e364c7560cdbb8
Gentle Calhoun <tj@Gentles-MacBook-Air.local>
2017-03-26 22:15:23 -0500

    Initial Commit

---
+++ homePage.php
@@ -0,0 +1,12 @@
+<!Doctype html>
+<html>
+<head>
+    <title>KSU Bookstore</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+<h4> Welcome to the Home Page Team! </h4>
+</body>
+</html>
